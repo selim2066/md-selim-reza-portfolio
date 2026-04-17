2d63015bfba3168d159ec884a291d6a5b31096a4
selim2066 <mdselimreza2066@gmail.com>
2026-04-17 16:47:06 +0600

    padding added

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -144,7 +144,7 @@ const profile = {
 
   return (
     <>
-      <main className="bg-[#020617] text-white min-h-screen">
+      <main className="bg-[#020617] text-white min-h-screen px-10">
         <section
           className="hero min-h-screen flex items-center justify-center relative px-4 sm:px-6 lg:px-8 py-10 md:py-16 lg:py-0 hero-section-padding"
           style={{ paddingTop: "var(--hero-padding-top, 0)" }}
